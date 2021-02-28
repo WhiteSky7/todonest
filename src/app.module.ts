98423ce3c32423e0f6790cf74b22a427a7fb89a6
WhiteSky7 <satalk77@gmail.com>
2021-02-28 11:00:14 +0300

    update app mpdule

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodosModule } from './todos/todos.module';
 import { SubtodosModule } from './subtodos/subtodos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
+
 
 @Module({
-  imports: [TodosModule, SubtodosModule],
+  imports: [TodosModule, SubtodosModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private connection: Connection) {}
+}
